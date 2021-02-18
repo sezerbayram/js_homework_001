b5b857656591443f9e9a04eac44f4de3c77de190
bayycesar <74101785+bayycesar@users.noreply.github.com>
2021-02-18 20:09:42 +0200

    update

---
+++ aync_await.js
@@ -0,0 +1,62 @@
+function marketeGit() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err;
+            if (!err) {
+                console.log("markete gidildi")
+                resolve();
+            }
+            else
+                reject("error")
+        }, 2000);
+    });
+};
+function alisverisYap() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err;
+            if (!err) {
+                let fruit = 'armut ve elma '
+                console.log('alisveris yapildi ' + fruit + 'alindi!!');
+                resolve();
+            }
+            else
+                reject("err")
+        }, 1000);
+    });
+
+};
+function eveDon() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err;
+            if (!err) {
+                console.log("eve donuldu")
+                resolve();
+            }
+            else
+                reject("error")
+        }, 500);
+    });
+};
+async function islemAkisi() {
+    await marketeGit()
+
+    .then((data) => {
+        
+        
+       alisverisYap()
+
+        .then((data) => {
+           
+            eveDon();        
+        })
+        .catch((hata) => {
+            console.log(hata);
+        });    
+    })
+    .catch((hata) => {
+        console.log(hata);
+    });
+}
+islemAkisi();
