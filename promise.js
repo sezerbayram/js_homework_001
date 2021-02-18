b7d805779115f4ea60ab572f169ab3961fd1a052
bayycesar <74101785+bayycesar@users.noreply.github.com>
2021-02-18 19:38:49 +0200

    update

---
+++ promise.js
@@ -0,0 +1,41 @@
+function marketeGit() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err;
+            if (!err) {
+                console.log("markete gidildi")
+                resolve();
+            }
+            else
+                reject("error")
+        }, 2000);
+    });
+};
+function alisverisYap() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err;
+            if (!err) {
+                let fruit = 'armut ve elma '
+                console.log('alisveris yapildi ' + fruit + 'alindi!!');
+                resolve();
+            }
+            else
+                reject("err")
+        }, 1000);
+    });
+
+};
+function eveDon() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let err;
+            if (!err) {
+                console.log("eve donuldu")
+                resolve();
+            }
+            else
+                reject("error")
+        }, 500);
+    });
+};
